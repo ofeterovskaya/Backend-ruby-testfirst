21c2b19694e1a76d3a0f1c4c306de944353395f8
Oksana Fetter <fetter.oksana@gmail.com>
2024-07-15 22:13:29 -0400

    ruby testing

--- 00_hello/hello.rb
+++ 00_hello/hello.rb
@@ -1,1 +1,11 @@
-#write your code here
+# hello.rb
+
+# Define the hello function that returns "Hello!"
+def hello
+    "Hello!"
+  end
+  
+  # Define the greet function that takes a name as an argument and returns a greeting string
+  def greet(who)
+    "Hello, #{who}!"
+  end
