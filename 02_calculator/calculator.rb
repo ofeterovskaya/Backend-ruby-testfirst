3619a80920b8a7f6a1508a625577f07a9a9a16ee
Oksana Fetter <fetter.oksana@gmail.com>
2024-07-22 00:56:47 -0400

    testing

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,28 @@
 #write your code here
+def add(a,b)
+    a + b
+end
+def subtract(a,b)
+    a - b
+end
+def sum(array)
+    array.inject(0, :+)
+end
+def multiply(array)
+    array.inject(1, :*)
+end
+def power(a,b)
+    a ** b
+end
+def factorial(n)
+    return 1 if n == 0
+    (1..n).inject { |product, num| product * num }
+  end
+  
+# def factorial(n)
+#     if n == 0
+#         1
+#     else
+#         (1..n).inject(:*)
+#     end
+# end
